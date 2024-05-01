e047574a2ece9c22f915755824ddf6f9d1f52c9e
vargazsolt0924 <vargazsolt1348@gmail.com>
2024-05-01 23:18:14 +0200

    egy kicsi változtatás

--- 1_Feladat/tests/example2.spec.js
+++ 1_Feladat/tests/example2.spec.js
@@ -3,24 +3,19 @@ const { test, expect } = require('@playwright/test');
 const URL = "https://wearecommunity.io/";
 
 test('Videó keresés', async ({ page }) => {
-    // Oldal megnyitása
+    
     await page.goto(URL);
     
-    // Várakozás az elem betöltődésére
     await page.waitForSelector('text="Videos"');
     
-    // Videók oldalra navigálás
     await Promise.all([
       page.click('text="Videos"'),
       page.waitForNavigation({ waitUntil: 'networkidle' }),
     ]);
   
-    // Keresőmező kitöltése és keresés végrehajtása
-    // Feltételezve, hogy a keresőmező `type="search"` attribútummal rendelkezik
     await page.fill('.evnt-search', 'English');
     await page.press('.evnt-search', 'Enter');
     
-    // Várakozás az eredmények megjelenésére
     await page.waitForSelector(`text="English"`);
 
   });
